b2285baa29699327ca45a7114fe0bbc90887561d
Matheus Donizete <math.d@live.com>
2018-11-20 22:22:10 -0300

    Update controller.cc

--- exercicio-b/controller.cc
+++ exercicio-b/controller.cc
@@ -7,7 +7,7 @@ using namespace std;
 
 /* Default constructor */
 Controller::Controller( const bool debug )
-  : debug_( debug ), windowSize(100), constant(2), lastRTT(100), packetCounter(0)
+  : debug_( debug ), windowSize(50), constant(3), lastRTT(100), packetCounter(0)
 {}
 
 /* Get current window size, in datagrams */
@@ -18,7 +18,7 @@ unsigned int Controller::window_size()
 
   if ( debug_ ) {
     cerr << "At time " << timestamp_ms()
-	 << " window size is " << the_window_size << endl;
+   << " window size is " << windowSize << endl;
   }
 
   return windowSize;
@@ -26,11 +26,11 @@ unsigned int Controller::window_size()
 
 /* A datagram was sent */
 void Controller::datagram_was_sent( const uint64_t sequence_number,
-				    /* of the sent datagram */
-				    const uint64_t send_timestamp,
+            /* of the sent datagram */
+            const uint64_t send_timestamp,
                                     /* in milliseconds */
-				    const bool after_timeout
-				    /* datagram was sent because of a timeout */ )
+            const bool after_timeout
+            /* datagram was sent because of a timeout */ )
 {
   /* Default: take no action */
   if(after_timeout) {
@@ -39,32 +39,33 @@ void Controller::datagram_was_sent( const uint64_t sequence_number,
 
   if ( debug_ ) {
     cerr << "At time " << send_timestamp
-	 << " sent datagram " << sequence_number << " (timeout = " << after_timeout << ")\n";
+   << " sent datagram " << sequence_number << " (timeout = " << after_timeout << ")\n";
   }
 }
 
 /* An ack was received */
 void Controller::ack_received( const uint64_t sequence_number_acked,
-			       /* what sequence number was acknowledged */
-			       const uint64_t send_timestamp_acked,
-			       /* when the acknowledged datagram was sent (sender's clock) */
-			       const uint64_t recv_timestamp_acked,
-			       /* when the acknowledged datagram was received (receiver's clock)*/
-			       const uint64_t timestamp_ack_received )
+             /* what sequence number was acknowledged */
+             const uint64_t send_timestamp_acked,
+             /* when the acknowledged datagram was sent (sender's clock) */
+             const uint64_t recv_timestamp_acked,
+             /* when the acknowledged datagram was received (receiver's clock)*/
+             const uint64_t timestamp_ack_received )
                                /* when the ack was received (by sender) */
 {
   /* Default: take no action */
   const uint64_t rtt = timestamp_ack_received - send_timestamp_acked;
-  windowSize = updateSize(windowSize, 1/constant, rtt >= lastRTT);  
+  windowSize = updateSize(windowSize, 1/constant, rtt >= 75); 
+  windowSize = windowSize <= 13 ? 13 : windowSize; 
   lastRTT = rtt;
 
   if ( debug_ ) {
     cerr << "At time " << timestamp_ack_received
-	 << " received ack for datagram " << sequence_number_acked
-	 << " (send @ time " << send_timestamp_acked
-	 << ", received @ time " << recv_timestamp_acked << " by receiver's clock)"
+   << " received ack for datagram " << sequence_number_acked
+   << " (send @ time " << send_timestamp_acked
+   << ", received @ time " << recv_timestamp_acked << " by receiver's clock)"
    << ", last RTT @" << lastRTT
-	 << endl;
+   << endl;
   }
 }
 
@@ -77,14 +78,16 @@ unsigned int Controller::timeout_ms()
 
 
 unsigned int Controller::updateSize(const uint64_t wTime, const uint64_t factor, const bool isConflict){
-  if(packetCounter < wTime) {
-    packetCounter++;
-    return wTime;
-  }
-  packetCounter = 0;
-  if(isConflict){
-    return wTime * factor;
-  }
+  if(packetCounter >= wTime) {    
+    packetCounter = 0;
+    if(isConflict){
+      return wTime * factor;
+    }
 
-  return wTime + 1;
+    return wTime + 1;
+  } 
+  
+
+  packetCounter++;
+  return wTime;
 }
